08d75cba1e115bbe7baa6d1f17889fd4538e4dca
nobleprogdwpterraform <nobleprogdwpterraform@gamil.com>
2026-02-24 22:39:25 +0000

    terraform

--- 5-Terrarform-State-Commands/main.tf
+++ 5-Terrarform-State-Commands/main.tf
@@ -4,11 +4,3 @@ resource "local_file" "pets" {
   content = "content"
 }
 
-/*
-1. terraform state list
-2. terraform state list local_file.pets
-3. terraform state show
-4. terraform state pull
-5. terraform state mv local_file.pets local_file.my-pets
-6. terraform state rm local_file.my-pets
-*/
--- 9-Terraform-with-VSphere/9-1-1-Vsphere-connection/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-# Sample values (not committed in real projects)
-
-vsphere_user     = "administrator@vsphere.local"
-vsphere_password = "VMware123!"
-vsphere_server   = "vcenter.mycorp.local"
